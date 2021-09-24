5af20979e5154805107c35d18cdf5514cee81d7b
Jack Lee <jack.lyhn.lee@gmail.com>
2021-09-24 01:12:04 -0700

    Added GAN models, training and checkpointing

---
+++ models.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def weights_init(w):
+    """Initializes the weights of the layer, w."""
+
+    classname = w.__class__.__name__
+    if classname.find("conv") != -1:
+        nn.init.normal_(w.weight.data, 0.0, 0.02)
+    elif classname.find("bn") != -1:
+        nn.init.normal_(w.weight.data, 1.0, 0.02)
+        nn.init.constant_(w.bias.data, 0)
+
+
+class Generator(nn.Module):
+    """Generator defines the generator network."""
+
+    def __init__(self, nz, ngf, nc):
+        super().__init__()
+        self.tconv1 = nn.ConvTranspose2d(
+            nz, ngf * 8, kernel_size=4, stride=1, padding=0, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(ngf * 8)
+        self.tconv2 = nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(ngf * 4)
+        self.tconv3 = nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False)
+        self.bn3 = nn.BatchNorm2d(ngf * 2)
+        self.tconv4 = nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False)
+        self.bn4 = nn.BatchNorm2d(ngf)
+        self.tconv5 = nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.tconv1(x)))
+        x = F.relu(self.bn2(self.tconv2(x)))
+        x = F.relu(self.bn3(self.tconv3(x)))
+        x = F.relu(self.bn4(self.tconv4(x)))
+        x = torch.tanh(self.tconv5(x))
+        return x
+
+
+class Discriminator(nn.Module):
+    """Discriminator defines the discriminator network."""
+
+    def __init__(self, nc, ndf):
+        super().__init__()
+        self.conv1 = nn.Conv2d(nc, ndf, 4, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(ndf * 2)
+        self.conv3 = nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False)
+        self.bn3 = nn.BatchNorm2d(ndf * 4)
+        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False)
+        self.bn4 = nn.BatchNorm2d(ndf * 8)
+        self.conv5 = nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.conv1(x), 0.2, True)
+        x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2, True)
+        x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2, True)
+        x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2, True)
+        x = torch.sigmoid(self.conv5(x))
+        return x
--- train.py
+++ train.py
@@ -1,14 +1,22 @@
-import urllib.request
 import os
 import tarfile
 import tempfile
-import pathlib
 import argparse
-from tqdm import tqdm
+import urllib.request
+
+import tqdm
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import models
+import trainer
 
 
 def parse_args():
     """Parses command line arguments."""
+
     root_dir = os.path.abspath(os.path.dirname(__file__))
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -53,13 +61,14 @@ def parse_args():
     parser.add_argument(
         "--seed", type=int, default=0, help="Manual seed for reproducibility."
     )
+
     return parser.parse_args()
 
 
 def download_data(
     dst_dir, url="http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar"
 ):
-    """Downloads and uncompresses the Stanford Dogs dataset."""
+    """Downloads and uncompresses the specified dataset."""
 
     def update_download_progress(blk_n=1, blk_sz=1, total_sz=None):
         assert update_download_progress.pbar is not None
@@ -70,23 +79,38 @@ def download_data(
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         tmp_path = os.path.join(tmp_dir, "compressed_data")
-        with tqdm(unit="B", unit_scale=True, desc="Downloading Dataset") as pbar:
+        with tqdm.tqdm(unit="B", unit_scale=True, desc="Downloading Dataset") as pbar:
             update_download_progress.pbar = pbar
             urllib.request.urlretrieve(
                 url, tmp_path, reporthook=update_download_progress
             )
         with tarfile.open(tmp_path, "r") as f:
-            for member in tqdm(f.getmembers(), desc="Extracting Dataset"):
+            for member in tqdm.tqdm(f.getmembers(), desc="Extracting Dataset"):
                 f.extract(member, path=dst_dir)
 
 
-def get_images(img_dir):
-    """Gets the paths of all images contained in directory."""
-    return list(pathlib.Path(img_dir).rglob("*.jpg"))
+def prepare_data(data_dir, imsize, batch_size):
+    """Creates a dataloader from a directory containing image data."""
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize(imsize),
+            transforms.CenterCrop(imsize),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    return torch.utils.data.DataLoader(
+        datasets.ImageFolder(root=data_dir, transform=transform),
+        batch_size=batch_size,
+        shuffle=True,
+    )
 
 
 def train(args):
     """Sets up environment, configures and trains model."""
+
     # Setup dataset
     if not os.path.exists(args.data_dir):
         download_data(args.data_dir)
@@ -107,10 +131,45 @@ def train(args):
         if not os.path.exists(d):
             os.mkdir(d)
 
-    # Configure models and trainer
+    # Fixed seed
+    torch.manual_seed(args.seed)
+
+    # Set parameters
+    nz, ngf, nc, ndf = 100, 64, 3, 64
+    imsize, batch_size = 64, 32
+    num_epochs, lr, betas, ckpt_every = 10, 0.0002, (0.5, 0.999), 500
+    criterion = nn.BCELoss()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Configure and initialize models
+    net_g = models.Generator(nz, ngf, nc).to(device)
+    net_g.apply(models.weights_init)
+    net_d = models.Discriminator(nc, ndf).to(device)
+    net_d.apply(models.weights_init)
+
+    # Configure dataloader and trainer
+    dataloader = prepare_data(args.data_dir, imsize, batch_size)
+    trainer_ = trainer.Trainer(
+        net_g,
+        net_d,
+        dataloader,
+        num_epochs,
+        nz,
+        lr,
+        betas,
+        criterion,
+        ckpt_every,
+        ckpt_dir,
+        log_dir,
+        device,
+    )
+
+    # Load last checkpoint if specified
+    if args.resume:
+        trainer_.load_checkpoint()
 
     # Train model
-    pass
+    trainer_.train()
 
 
 if __name__ == "__main__":
