c97a6dccb25752b2ff9f2d2e9258b8cf0dff927b
jacklyonlee <jacklyonlee@gmail.com>
2021-09-24 17:22:17 -0700

    Finished and tested DCGAN baseline

--- models.py
+++ models.py
@@ -19,25 +19,28 @@ class Generator(nn.Module):
 
     def __init__(self, nz, ngf, nc):
         super().__init__()
-        self.tconv1 = nn.ConvTranspose2d(
-            nz, ngf * 8, kernel_size=4, stride=1, padding=0, bias=False
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(
+                nz, ngf * 8, kernel_size=4, stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh(),
         )
-        self.bn1 = nn.BatchNorm2d(ngf * 8)
-        self.tconv2 = nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(ngf * 4)
-        self.tconv3 = nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(ngf * 2)
-        self.tconv4 = nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False)
-        self.bn4 = nn.BatchNorm2d(ngf)
-        self.tconv5 = nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False)
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.tconv1(x)))
-        x = F.relu(self.bn2(self.tconv2(x)))
-        x = F.relu(self.bn3(self.tconv3(x)))
-        x = F.relu(self.bn4(self.tconv4(x)))
-        x = torch.tanh(self.tconv5(x))
-        return x
+        out = self.net(x)
+        return out
 
 
 class Discriminator(nn.Module):
@@ -45,19 +48,22 @@ class Discriminator(nn.Module):
 
     def __init__(self, nc, ndf):
         super().__init__()
-        self.conv1 = nn.Conv2d(nc, ndf, 4, 2, 1, bias=False)
-        self.conv2 = nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(ndf * 2)
-        self.conv3 = nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(ndf * 4)
-        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False)
-        self.bn4 = nn.BatchNorm2d(ndf * 8)
-        self.conv5 = nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False)
+        self.net = nn.Sequential(
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x), 0.2, True)
-        x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2, True)
-        x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2, True)
-        x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2, True)
-        x = torch.sigmoid(self.conv5(x))
-        return x
+        out = self.net(x)
+        return out
